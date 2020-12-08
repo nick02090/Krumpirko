57eb65eb5606f9f25677a983af797a35e0d19a11
Otto <otto.engelmann.official@gmail.com>
2020-12-08 22:52:56 +0100

    AI test scene and simple EnemyController

---
+++ Assets/Scripts/AI/EnemyController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    public Transform player;
+    public NavMeshAgent agent;
+
+    void Start()
+    {
+        agent.updateRotation = false;
+    }
+
+    void Update()
+    {
+        agent.SetDestination(player.position);
+    }
+}
