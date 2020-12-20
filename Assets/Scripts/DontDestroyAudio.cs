51ea5e6175aa49dce98c22b4135614d416c170c2
Ante Grbesa <ante214@gmail.com>
2020-12-20 16:03:10 +0000

    Add music to main menu

---
+++ Assets/Scripts/DontDestroyAudio.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyAudio : MonoBehaviour
+{
+
+    static DontDestroyAudio instance;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+    }
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,7 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-   public void PlayGame()
+
+   public int _selectedLevel = 1;
+ 
+   public void PlayLevel1()
+    {
+        _selectedLevel = 1;
+        Play();
+    }
+
+
+    public void PlayLevel2()
+    {
+        _selectedLevel = 2;
+        Play();
+    }
+
+
+    public void PlayLevel3()
+    {
+        _selectedLevel = 3;
+        Play();
+
+    }
+
+    private void Play()
     {
         SceneManager.LoadScene("SampleScene");
     }
