20f3536150bf257404ce2e8ad837b4be1dbd9c9b
Nikola Ćališ <nidjolas.u.b@gmail.com>
2020-12-20 16:43:38 +0100

    Show available actions in the HUD

--- Assets/Scripts/UI/HUD.cs
+++ Assets/Scripts/UI/HUD.cs
@@ -1,12 +1,14 @@
-﻿using Gameplay.Characters;
+﻿using Gameplay.Actions;
+using Gameplay.Characters;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HUD : MonoBehaviour
 {
-    public UnityEngine.UI.Text ScoreText;
+    public Text ScoreText;
     public RectTransform Actions;
 
-    private ICharacter playerCharacter;
+    private PlayerCharacter playerCharacter;
 
     private void Start()
     {
@@ -17,5 +19,44 @@ public class HUD : MonoBehaviour
     private void Update()
     {
         ScoreText.text = $"Score: {playerCharacter.GetCurrentScore()}";
+
+        for (int i = 0; i < Actions.childCount; i++)
+        {
+            Transform action = Actions.GetChild(i);
+            switch (action.name)
+            {
+                case "Bait":
+                    UpdateActionIcon(action, ActionType.Bait);
+                    break;
+                case "Cough":
+                    UpdateActionIcon(action, ActionType.Cough);
+                    break;
+                case "Gum":
+                    UpdateActionIcon(action, ActionType.Gum);
+                    break;
+                case "HotSauce":
+                    UpdateActionIcon(action, ActionType.HotSauce);
+                    break;
+                case "Ketchup":
+                    UpdateActionIcon(action, ActionType.Ketchup);
+                    break;
+                default:
+                    Debug.LogError("HUD::Invalid action name!");
+                    break;
+            }
+        }
+    }
+
+    private void UpdateActionIcon(Transform action, ActionType actionType)
+    {
+        Image actionImage = action.GetComponent<Image>();
+        if (playerCharacter.CanExecuteAction(actionType))
+        {
+            actionImage.color = new Color(1.0f, 1.0f, 1.0f);
+        }
+        else
+        {
+            actionImage.color = new Color(0.5f, 0.5f, 0.5f);
+        }
     }
 }
